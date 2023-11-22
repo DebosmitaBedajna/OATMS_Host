bfcc0409b2ec1722e783a2e23d2ef57dd0782f99
Debosmita Bedajna <bedajnadebosmita@gmail.com>
2023-11-22 23:31:40 +0530

    flask

--- app.py
+++ app.py
@@ -281,4 +281,4 @@ def get_scheduled_flights():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, host='127.0.0.1', port=5000, debug=True, use_reloader=False)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, use_reloader=False)
